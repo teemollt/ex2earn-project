29dcd0685b613b7cfc43214d472e9dfd9807bc25
Youngkwon-Lee <99164652+Youngkwon-Lee@users.noreply.github.com>
2025-03-01 19:46:28 +0900

    Initial commit: Project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/login" element={<div>Login</div>} />
+        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/dashboard" element={<div>Dashboard</div>} />
+        <Route path="/squat-challenge" element={<div>Squat Challenge</div>} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
